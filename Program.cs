6e8f817221deb6553f96580d5db02dc6992caf32
chrisbola <bolachristian8@gmail.com>
2021-06-22 13:11:46 +0100

    My new git push

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x =10;
+            int y =3;
+            int result = x % y;
+
+            Console.WriteLine("result = " + result);
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, (x + y));
+        }
+    }
+}
